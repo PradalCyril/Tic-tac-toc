93aa9aa7c0abe30e3fe4475da4d473f07963d696
PradalCyril <pradal.cyril@gmail.com>
2020-07-10 11:08:29 +0200

    - Fix cells change icon - remove player turn if game was finished. - remove game board if player win.

--- src/components/GamePage/Cell/Cell.js
+++ src/components/GamePage/Cell/Cell.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import croix from '../../../assets/croix.svg';
+import rond from '../../../assets/rond.svg';
 
-const Cell = (props) => {
-    const {haveWinner, name, className, cells, endTurn} = props; 
-    return (
-        <div
-            className={className}
-            onClick={() => endTurn({ name, isWin: haveWinner.stopGame })}
-        >
-            {cells[name].icon}
-        </div>
-    )
+class Cell extends Component {
+    state = {
+        isUsed: false,
+        icon: (<div></div>)
+    }
+
+    endTurn = () => {
+        const { playerNameTurn, name1, endTurn, name, haveWinner } = this.props;
+        if (haveWinner.stopGame || this.state.isUsed) return;
+        let icon;
+           if (playerNameTurn !== name1) {
+            icon = (<img src={croix} className="player2-img" alt="croix" />);
+        } else {
+            icon = (<img src={rond} className="player2-img" alt="rond" />);
+        }
+        this.setState({icon, isUsed: true});
+        endTurn({ name, isWin: haveWinner.stopGame });
+    }
+
+    render() {
+        const { name, className } = this.props;
+
+        return (
+            <div
+                id={`cell-${name}`}
+                className={className}
+                onClick={() => this.endTurn()}
+            >
+                {this.state.icon}
+            </div>
+        )
+    }
 }
 
 export default Cell; 
